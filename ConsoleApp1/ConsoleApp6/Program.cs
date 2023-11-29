a2e9aeb1a3a8071cc27fe31ca33133e6b4585f85
drost-dev <andrey.06.ezdakov@gmail.com>
2023-11-30 01:29:58 +0700

    app6 edited

--- ConsoleApp1/ConsoleApp6/Program.cs
+++ ConsoleApp1/ConsoleApp6/Program.cs
@@ -2,37 +2,121 @@
 //Console.WriteLine("uncomment code");
 
 /*  №1
-Дан файл numsTask1.txt со словами. Выведите все слова нечетной длины;
+string[] words = File.ReadAllLines("numsTask1.txt")[0].Split(' ');
+foreach (var w in words)
+{
+    if (w.Length % 2 != 0)
+    {
+        Console.WriteLine(w);
+    }
+}
 */
-
-
 
 //-----------------------------------------------------------------------------
 
 /*  №2
-Дан файл numsTask2.txt со словами расположенными в столбик. Из заданных
-слов составьте одну длинную строку(разделить каждое слово пробелом);
+string[] lines = File.ReadAllLines("numsTask2.txt");
+foreach (var l in lines)
+{
+    Console.Write($"{l} ");
+}
 */
 
 //-----------------------------------------------------------------------------
 
 /*  №3
-Дано число. Определите будет ли это число четным и кратным 10;
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0 && num % 10 == 0)
+{
+    Console.WriteLine($"Число {num} четное и кратно десяти");
+}
+else
+{
+    Console.WriteLine($"Число {num} не четное и/или не кратно десяти");
+}
 */
 
 //-----------------------------------------------------------------------------
 
 /*  №4
-Вводятся положительные числа. Определите сумму чисел, делящихся на
-положительное число a нацело. При вводе отрицательного числа закончите работу;
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1)
+{
+    Console.WriteLine("Число a не может быть отрицательным!");
+    return;
+}
+
+int sum = 0;
+Console.WriteLine("Вводите числа, для завершения введите 0:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num != 0)
+{
+    if (num < 0)
+    {
+        Console.WriteLine("Числа не могут быть отрицательными!");
+        return;
+    }
+
+    if (num % a == 0)
+    {
+        sum += num;
+    }
+    
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(sum);
 */
 
 //-----------------------------------------------------------------------------
 
 /*  №5
-Дана прямоугольная матрица a, имеющей n строк и m столбцов. Исходная
-матрица состоит из нулей и единиц. Добавьте к матрице еще один столбец, каждый
-элемент которого делает количество единиц в каждой строке четным;
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] a = new int[n, m];
+int[,] a2 = new int[n, m + 1];
+
+Random r = new Random();
+
+int onesAmount = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        a[i, j] = r.Next(0, 2);
+        a2[i, j] = a[i, j];
+        if (a2[i, j] == 1)
+        {
+            onesAmount++;
+        }
+        Console.Write($"{a[i, j]} ");
+    }
+
+    if (onesAmount % 2 != 0)
+    {
+        a2[i, m] = 1;
+    }
+    else
+    {
+        a2[i, m] = 0;
+    }
+
+    onesAmount = 0;
+
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= m; j++)
+    {
+        Console.Write($"{a2[i, j]} ");
+    }
+    
+    Console.WriteLine();
+}
 */
 
 //-----------------------------------------------------------------------------
