a68b1ccd94f42da671a8c2063a73beeb3b26b250
drost-dev <andrey.06.ezdakov@gmail.com>
2023-12-03 22:44:46 +0700

    app8 created

---
+++ ConsoleApp1/ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+
+namespace ConsoleApp8;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.weather.yandex.ru/v2/forecast?lat=55.75396&lon=37.620393&extra=true");
+        request.Method = "GET";
+        request.Headers.Add("X-Yandex-API-Key", "73400b68-9299-4e43-9068-8da1156c96ed");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        Stream stream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(stream);
+        string jsonString = reader.ReadToEnd();
+
+        response.Close();
+        Console.WriteLine(jsonString);
+    }
+}
